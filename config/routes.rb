63e897a6cc2f9a8f80237f4ef9db6465cc03136e
Giuseppe Trovato <giusi.trovato@me.com>
2022-07-09 17:06:43 +0200

    added-controller-and-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookmarks do
-    # resources :reviews, only: [ :new, :create ]
-  end
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show', as: :list
 end
